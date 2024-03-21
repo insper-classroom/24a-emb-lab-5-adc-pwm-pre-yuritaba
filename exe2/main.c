5cfd2ea5298b67e0b8c02c41090af669048423d5
yuritaba <yurit@al.insper.edu.br>
2024-03-21 17:22:11 +0000

    2

--- exe2/main.c
+++ exe2/main.c
@@ -8,23 +8,28 @@
 #include "hardware/pwm.h"
 
 const int PWM_0_PIN = 4;
-const int PWM_1_PIN = 6; // Assumindo que o pino do LED G é 5, ajuste conforme necessário
+const int PWM_1_PIN = 6;
 
 void led_task(void *p) {
-    // Configuração para o LED R
     gpio_set_function(PWM_0_PIN, GPIO_FUNC_PWM);
-    uint slice_num_r = pwm_gpio_to_slice_num(PWM_0_PIN);
-    pwm_set_clkdiv(slice_num_r, 125);
-    pwm_set_wrap(slice_num_r, 100);
-    pwm_set_chan_level(slice_num_r, PWM_CHAN_A, 80);
-    pwm_set_enabled(slice_num_r, true);
+    uint slice_num = pwm_gpio_to_slice_num(PWM_0_PIN);
+    pwm_set_clkdiv(slice_num, 125);
+    pwm_set_wrap(slice_num, 100);
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, 80);
+    pwm_set_enabled(slice_num, true);
 
-    // Configuração para o LED G
+    while (true) {
+    }
+}
+
+
+void led_task2(void *p) {
     gpio_set_function(PWM_1_PIN, GPIO_FUNC_PWM);
-    uint slice_num_g = pwm_gpio_to_slice_num(PWM_1_PIN);
-    // Assume que o LED G está no mesmo slice e usa o mesmo clkdiv e wrap
-    pwm_set_chan_level(slice_num_g, PWM_CHAN_B, 20); // Duty cycle de 20%
-    pwm_set_enabled(slice_num_g, true);
+    uint slice_num = pwm_gpio_to_slice_num(PWM_1_PIN);
+    pwm_set_clkdiv(slice_num, 125);
+    pwm_set_wrap(slice_num, 100);
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, 20);
+    pwm_set_enabled(slice_num, true);
 
     while (true) {
     }
@@ -35,6 +40,7 @@ int main() {
     printf("Start RTOS \n");
 
     xTaskCreate(led_task, "LED_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(led_task2, "LED_Task 2", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
